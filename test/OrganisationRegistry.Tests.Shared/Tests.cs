e28c2fc51bb304e1d1c9513d42d2a52524ce583e
Koen Metsu <koen@kometsoft.be>
2019-11-22 14:10:13 +0100

    feat: add test to Tests.Shared

---
+++ test/OrganisationRegistry.Tests.Shared/Tests.cs
@@ -0,0 +1,13 @@
+namespace OrganisationRegistry.Tests.Shared
+{
+    using Xunit;
+
+    public class Tests
+    {
+        [Fact]
+        public void SanityCheck()
+        {
+            Assert.NotEqual(1, 2);
+        }
+    }
+}
